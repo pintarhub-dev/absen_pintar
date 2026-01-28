c8ac0b6b68682585ae3021a0bfc7e706febc6289
pintarhub-dev <pintarhub@gmail.com>
2026-01-28 12:09:34 +0700

    Penambahan validasi untuk bypass url edit

--- app/Filament/Resources/LeaveRequestResource/Pages/EditLeaveRequest.php
+++ app/Filament/Resources/LeaveRequestResource/Pages/EditLeaveRequest.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\LeaveRequestResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use App\Models\LeaveRequest;
+use Filament\Notifications\Notification;
 
 class EditLeaveRequest extends EditRecord
 {
@@ -14,6 +15,20 @@ class EditLeaveRequest extends EditRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function beforeFill(): void
+    {
+        if ($this->record->status !== 'pending') {
+
+            Notification::make()
+                ->title('Akses Ditolak')
+                ->body('Pengajuan yang sudah diproses (Approved/Rejected) tidak dapat diedit lagi.')
+                ->warning()
+                ->send();
+
+            $this->redirect($this->getResource()::getUrl('index'));
+        }
     }
 
     protected function getHeaderActions(): array
--- app/Filament/Resources/OvertimeRequestResource/Pages/EditOvertimeRequest.php
+++ app/Filament/Resources/OvertimeRequestResource/Pages/EditOvertimeRequest.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\OvertimeRequestResource;
 use App\Models\OvertimeRequest;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
 
 class EditOvertimeRequest extends EditRecord
 {
@@ -14,6 +15,20 @@ class EditOvertimeRequest extends EditRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function beforeFill(): void
+    {
+        if ($this->record->status !== 'pending') {
+
+            Notification::make()
+                ->title('Akses Ditolak')
+                ->body('Pengajuan yang sudah diproses (Approved/Rejected) tidak dapat diedit lagi.')
+                ->warning()
+                ->send();
+
+            $this->redirect($this->getResource()::getUrl('index'));
+        }
     }
 
     protected function getHeaderActions(): array
